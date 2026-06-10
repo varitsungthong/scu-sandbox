27d994ba57d088ba6021fa0f33e8c5dd2dc351c4
varitsungthong <varitsungthong@gmail.com>
2026-06-10 15:34:30 +0700

    Chapter 3.3 : Functions, Statements and Expressions
    understanding differences between statement and expression

--- rust_learning/chapter_3_common_programming_concepts/chapter_3_3_Functions/src/main.rs
+++ rust_learning/chapter_3_common_programming_concepts/chapter_3_3_Functions/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    println!("Hello, world!");
-    another_function(5,'h');
-}
+    let y = {
+        let x = 3;
+        x + 1
+    };
 
-fn another_function(x: i32, y: char) {
-    println!("The value of x is {x}{y}.");
+    println!("The value of y is: {y}");
+
+//statement doesnt return value
+//Expressions do not include ending semicolons if semicolons are added it will turn to statement
 }
