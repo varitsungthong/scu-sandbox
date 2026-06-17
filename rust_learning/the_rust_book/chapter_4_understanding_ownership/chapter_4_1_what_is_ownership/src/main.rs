0027d17171bd6067e091b0c9c1deb8b20c4a1e60
varitsungthong <varitsungthong@gmail.com>
2026-06-17 18:36:06 +0700

    feat(the rust book) : chapter 4.1 copy trait and drop trait
     in rust some type of data have 'copy' trait which is mutually exclusive to 'drop' trait

--- rust_learning/the_rust_book/chapter_4_understanding_ownership/chapter_4_1_what_is_ownership/src/main.rs
+++ rust_learning/the_rust_book/chapter_4_understanding_ownership/chapter_4_1_what_is_ownership/src/main.rs
@@ -1,5 +1,9 @@
-fn main () {
-    let s1 =String::from("Hello");
-    let s2 = s1.clone();
-    println!("s1 = {s1} and s2 = {s2}")
+fn main (){
+    let x = 5;
+    let y = x;
+
+    println!("x = {x}, y = {y}");
+    //in rust some data have "copy" trait and will copy data instead of moving it
+    //if the data have "copy" trait it cant have "drop trait"
+    //the value that have copy tait are boolean types, integer types, floating types, character type and some tuple type
 }
