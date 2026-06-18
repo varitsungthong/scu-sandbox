8912d8d3609e187b7aec5cf265b200dd891e2c48
varitsungthong <varitsungthong@gmail.com>
2026-06-18 22:14:42 +0700

    feat(the rust book) : chapter 4.2, multiple mutable references

--- rust_learning/the_rust_book/chapter_4_understanding_ownership/chapter_4_2_references_and_borrowing/src/main.rs
+++ rust_learning/the_rust_book/chapter_4_understanding_ownership/chapter_4_2_references_and_borrowing/src/main.rs
@@ -1,8 +1,20 @@
+    
 fn main (){
     let mut s = String::from("hello");
-    change(&mut s);
-    println!("{s}")
-}
-fn change(some_string : &mut String){
-    some_string.push_str(", world")
+
+    {
+        let r1 = &mut s;
+         println!("{r1}")
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    
+   
+
+    let r3 = &s;
+    let r4 = &s;
+
+    println!("{r3},{r4}")
+    // Multiple immutable references are allowed because
+    // none of them can modify the data.
+    //if two or more pointer access data at the same time without synchronizer it may cause data race 
+
 }
