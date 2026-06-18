373a3dfe35486428c7def85f2d7de905b99b5c36
varitsungthong <varitsungthong@gmail.com>
2026-06-18 22:02:45 +0700

    feat(the rust book) : chapter 4.2, mutable reference.

--- rust_learning/the_rust_book/chapter_4_understanding_ownership/chapter_4_2_references_and_borrowing/src/main.rs
+++ rust_learning/the_rust_book/chapter_4_understanding_ownership/chapter_4_2_references_and_borrowing/src/main.rs
@@ -1,10 +1,8 @@
 fn main (){
-    let s1 = String::from("Hello"); 
-    let len = calculate_length(&s1); //borrow s1 ownership
-                                     //this is borrowing, the reference data cant be modify by default
-
-    println!("The length of '{s1}' is {len}."); //s1 is still valid and len is return form function
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{s}")
 }
-fn calculate_length(s: &String) -> usize{ //&string is crucial without & rust will expect string no reference
-    s.len() //expression return usize
+fn change(some_string : &mut String){
+    some_string.push_str(", world")
 }
