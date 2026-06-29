36ffdc8bfa0ef535296e81d8e22edb179f16804e
varitsungthong <varitsungthong@gmail.com>
2026-06-29 19:05:11 +0700

    feat(the rust book) : Chapter 8, Using an Enum to Store Multiple Types. Defining an enum to store values of different types in one vector.

--- rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_1_vectors/src/main.rs
+++ rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_1_vectors/src/main.rs
@@ -1,9 +1,19 @@
+#[derive(Debug)]
+ enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
 
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50 ;
-        println!("{i}");
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+        for cell in &row {
+        println!("{:?}", cell);
     }
 
 }
