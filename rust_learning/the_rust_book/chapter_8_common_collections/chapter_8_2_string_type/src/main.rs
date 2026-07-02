207b168637649b0e6af49d01c39e246e71f73e81
varitsungthong <varitsungthong@gmail.com>
2026-07-02 11:01:35 +0700

    feat(the rust book) : Chapter 8, Storing UTF-8 Encoded Text with Strings. Updating a String
    concatenate multiple strings using the 'format!'.

--- rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_2_string_type/src/main.rs
+++ rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_2_string_type/src/main.rs
@@ -3,6 +3,6 @@ fn main() {
     let s2 = String::from("tac");
     let s3 = String::from("toe");
 
-    let s = s1 + "-" + &s2 + "-" + &s3;
+    let s = format!("{s1}-{s2}-{s3}");
     println!("{}", s)
 }
