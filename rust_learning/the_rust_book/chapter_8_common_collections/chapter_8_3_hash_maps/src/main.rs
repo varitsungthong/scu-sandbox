fd2fe4f891a57ea05d70ec1cbbe362a9244824a6
varitsungthong <varitsungthong@gmail.com>
2026-07-02 11:34:51 +0700

    feat(the rust book) : Chapter 8, Storing Keys with Associated Values in Hash Maps. Accessing Values in a Hash Map.
    Accessing the score for the Blue team stored in the hash map.

--- rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_3_hash_maps/src/main.rs
+++ rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_3_hash_maps/src/main.rs
@@ -1,8 +1,15 @@
 use std::collections::HashMap;
 
 fn main() {
+
     let mut scores = HashMap::new();
+
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
-    println!("{:#?}", scores);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+      println!("{:#?}", scores);
+      println!("{}", team_name);
+      println!("{}", score);
 }
