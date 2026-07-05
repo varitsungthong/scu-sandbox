228a01c3bd06467225d2c2166c7a88004e073289
varitsungthong <varitsungthong@gmail.com>
2026-07-05 13:50:13 +0700

    feat(the rust book) : Chapter 8, Storing Keys with Associated Values in Hash Maps. Updating a Hashmap : Overwriting a Value.
     Replacing a value stored with a particular key.

--- rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_3_hash_maps/src/main.rs
+++ rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_3_hash_maps/src/main.rs
@@ -3,14 +3,11 @@ fn main() {
 
     use std::collections::HashMap;
 
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);;
-        
-    for (key, value) in &map {
-        println!("{key}: {value}");
-    }
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
     
 }
