0f86f3d4ead479f7bb603c1f25c11fdd9160188d
varitsungthong <varitsungthong@gmail.com>
2026-07-05 14:12:53 +0700

    feat(the rust book) : Chapter 8, Storing Keys with Associated Values in Hash Maps. Updating a Hashmap : Updating a Value Based on the Old Value.
     Counting occurrences of words using a hash map that stores words and counts.

--- rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_3_hash_maps/src/main.rs
+++ rust_learning/the_rust_book/chapter_8_common_collections/chapter_8_3_hash_maps/src/main.rs
@@ -2,12 +2,12 @@
 fn main() {
 
     use std::collections::HashMap;
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+      let count = map.entry(word).or_insert(0) ;
+      *count += 1;
+    }
 
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-
-    println!("{scores:?}");
+    println!("{map:?}");
 }
